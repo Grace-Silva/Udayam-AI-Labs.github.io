fad97034ed2ea66fb2f9c109a9eaec521f664b00
Grace <gracesilvaduarte@gmail.com>
2025-12-10 11:49:13 -0600

    funcionalidad de descarga añadida

---
+++ docs/js/free-courses-data.js
@@ -0,0 +1,130 @@
+// Constante gigante con todos los datos de los COURSES
+const COURSES = {
+  "html-css-basico": {
+    id: "html-css-basico",
+    language: "English",
+    courseTitle: "HTML y CSS desde Cero",
+    courseDescription:
+      "Aprende los fundamentos del desarrollo web frontend con proyectos prácticos y ejemplos reales.",
+    courseImage:
+      "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=400&h=200&fit=crop",
+    courseDuration: "4 hours",
+    courseVideos: [
+      {
+        id: "video1",
+        title: "Introducción a HTML y estructura web",
+        youtubeId: "rbuYtrNUxg4",
+        youtubeImage: "https://img.youtube.com/vi/rbuYtrNUxg4/hqdefault.jpg",
+        visto: false,
+      },
+      {
+        id: "video2",
+        title: "Estructura semántica HTML5",
+        youtubeId: "WWw7ZzNGXew",
+        youtubeImage: "https://img.youtube.com/vi/WWw7ZzNGXew/hqdefault.jpg",
+        visto: false,
+      },
+      {
+        id: "video3",
+        title: "CSS Grid y Flexbox moderno",
+        youtubeId: "yfoY53QXEnI",
+        youtubeImage: "https://img.youtube.com/vi/yfoY53QXEnI/hqdefault.jpg",
+        visto: false,
+      },
+      {
+        id: "video4",
+        title: "Diseño responsive y mobile-first",
+        youtubeId: "phWxA89Dy94",
+        youtubeImage: "https://img.youtube.com/vi/phWxA89Dy94/hqdefault.jpg",
+        visto: false,
+      },
+    ],
+  },
+  "javascript-fundamentos": {
+    id: "javascript-fundamentos",
+    language: "English",
+    courseTitle: "JavaScript para Principiantes",
+    courseDescription:
+      "Domina los conceptos esenciales de JavaScript con ejercicios prácticos y proyectos reales.",
+    courseImage:
+      "https://images.unsplash.com/photo-1627398242454-45a1465c2479?w=400&h=200&fit=crop",
+    courseDuration: "5 hours",
+    courseVideos: [
+      {
+        id: "js1",
+        title: "Variables, tipos de datos y operadores",
+        youtubeId: "RqQ1d1qEWlE",
+        youtubeImage: "https://img.youtube.com/vi/RqQ1d1qEWlE/hqdefault.jpg",
+        visto: false,
+      },
+      {
+        id: "js2",
+        title: "Funciones, scope y closures",
+        youtubeId: "Mus_vwhTCq0",
+        youtubeImage: "https://img.youtube.com/vi/Mus_vwhTCq0/hqdefault.jpg",
+        visto: false,
+      },
+      {
+        id: "js3",
+        title: "Manipulación del DOM y eventos",
+        youtubeId: "lTSJRYrAkl4",
+        youtubeImage: "https://img.youtube.com/vi/lTSJRYrAkl4/hqdefault.jpg",
+        visto: false,
+      },
+      {
+        id: "js4",
+        title: "Arrays, objetos y métodos",
+        youtubeId: "mEtlbH6OizI",
+        youtubeImage: "https://img.youtube.com/vi/mEtlbH6OizI/hqdefault.jpg",
+        visto: false,
+      },
+      {
+        id: "js5",
+        title: "ES6+ features modernas",
+        youtubeId: "8FmBEN0XZyI",
+        youtubeImage: "https://img.youtube.com/vi/8FmBEN0XZyI/hqdefault.jpg",
+        visto: false,
+      },
+    ],
+  },
+  "python-introduccion": {
+    id: "python-introduccion",
+    language: "English",
+    courseTitle: "Python para Todos",
+    courseDescription:
+      "Introducción a la programación con Python, desde cero hasta tus primeros proyectos.",
+    courseImage:
+      "https://images.unsplash.com/photo-1526379879527-8559ecfcaec0?w=400&h=200&fit=crop",
+    courseDuration: "6 hours",
+    courseVideos: [
+      {
+        id: "py1",
+        title: "Instalación y primer programa",
+        youtubeId: "kqtD5dpn9C8",
+        youtubeImage: "https://img.youtube.com/vi/kqtD5dpn9C8/hqdefault.jpg",
+        visto: false,
+      },
+      {
+        id: "py2",
+        title: "Estructuras de control y loops",
+        youtubeId: "HGOBQPFzWKo",
+        youtubeImage: "https://img.youtube.com/vi/HGOBQPFzWKo/hqdefault.jpg",
+        visto: false,
+      },
+      {
+        id: "py3",
+        title: "Funciones y módulos en Python",
+        youtubeId: "9Os0o3bizP8",
+        youtubeImage: "https://img.youtube.com/vi/9Os0o3bizP8/hqdefault.jpg",
+        visto: false,
+      },
+      {
+        id: "py4",
+        title: "Manipulación de archivos y JSON",
+        youtubeId: "jjq1VEc-1o0",
+        youtubeImage: "https://img.youtube.com/vi/jjq1VEc-1o0/hqdefault.jpg",
+        visto: false,
+      },
+    ],
+  },
+};
